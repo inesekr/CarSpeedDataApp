beb7e1e9bdee42b53a1adc71a19933797000c146
Inese Krole <inese.krole@gmail.com>
2023-11-10 14:06:36 +0200

    Cleaning done.

--- NexallApp/DataLoaderService.cs
+++ NexallApp/DataLoaderService.cs
@@ -13,20 +13,7 @@
         {
             var batchSize = 1000;
             var lines = File.ReadAllLines(filePath);
-            //foreach (var line in lines)
-            //{
-            //    var dataParts = line.Split('\t');
-            //    if (dataParts.Length >= 3)
-            //    {
-            //        var carData = new CarData
-            //        {
-            //            Date = DateTime.Parse(dataParts[0]),
-            //            Speed = int.Parse(dataParts[1]),
-            //            CarRegNo = dataParts[2]
-            //        };
-            //        _context.CarData.Add(carData);
-            //    }
-            //}
+           
             for (int i = 0; i < lines.Length; i += batchSize)
             {
                 var batchLines = lines.Skip(i).Take(batchSize);
--- README.md
+++ README.md
@@ -7,11 +7,11 @@
 #### Faila ielādes laiks
 ---
 ### Potenciālie uzlabojumi:
-#### Koda organizācija mapēs, funkciju iznešana atsevišķās klasēs - īpaši ja palielinās projekta apjoms (nepieciešams ieviest papildus skatus, funkcionalitāti utt).
+#### 1. Papildus validācijas, kļūdu paziņojumi utt.
+#### 2. Koda organizācija mapēs, funkciju iznešana atsevišķās klasēs - īpaši ja palielinās projekta apjoms (nepieciešams ieviest papildus skatus, funkcionalitāti utt).
 ---
 #### Projekta palaišanas instrukcija:
-* Atvērt ZIP, extract files, select folder
-* Atvērt ar Visual Studio, palaist solution failu no NexallApp mapes
+* Atvērt projekta folderi no Visual Studio, palaist NexallApp.sln solutions failu.
 * Palaist (run) projektu, atvērsies uz localhost.
 ---
 
